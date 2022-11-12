92baf0f72aeb955d06cc867c52c91c9f3fe8cef2
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2022-11-12 14:39:12 +0300

    Create Kubuntu_Vultr.sh

---
+++ Kubuntu_Vultr.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -e
+
+# https://linuxize.com/post/how-to-install-xrdp-on-ubuntu-20-04/
+# https://www.hiroom2.com/ubuntu-2004-xrdp-kde-en
+
+sudo apt install kubuntu-desktop xrdp -y
+sudo adduser xrdp ssl-cert
+sudo systemctl restart xrdp
+sudo ufw allow 3389
+
+sudo sed -e 's/^new_cursors=true/new_cursors=false/g' \
+     -i /etc/xrdp/xrdp.ini
+sudo systemctl enable xrdp
+sudo systemctl restart xrdp
+
+echo "startplasma-x11" > ~/.xsession
+D=/usr/share/plasma:/usr/local/share:/usr/share:/var/lib/snapd/desktop
+C=/etc/xdg/xdg-plasma:/etc/xdg
+C=${C}:/usr/share/kubuntu-default-settings/kf5-settings
+cat <<EOF > ~/.xsessionrc
+export XDG_SESSION_DESKTOP=KDE
+export XDG_DATA_DIRS=${D}
+export XDG_CONFIG_DIRS=${C}
+EOF
+
+cat <<EOF | \
+  sudo tee /etc/polkit-1/localauthority/50-local.d/xrdp-NetworkManager.pkla
+[xrdp-Netowrkmanager]
+Identity=unix-group:sudo
+Action=org.freedesktop.NetworkManager.network-control
+ResultAny=no
+ResultInactive=yes
+ResultActive=yes
+EOF
+
+cat <<EOF | \
+  sudo tee /etc/polkit-1/localauthority/50-local.d/xrdp-packagekit.pkla
+[xrdp-packagekit]
+Identity=unix-group:sudo
+Action=org.freedesktop.packagekit.system-sources-refresh
+ResultAny=no
+ResultInactive=yes
+ResultActive=yes
+EOF
+
+sudo systemctl restart polkit
+sudo systemctl restart xrdp
+
+
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+## Dil Sorunu » https://forum.debian.org.tr/index.php?topic=4712.0
