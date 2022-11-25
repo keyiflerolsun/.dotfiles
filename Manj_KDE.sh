0366301dfecca1caa22829679573cb18b9ef97d1
keyiflerolsun <keyiflerolsun@gmail.com>
2022-11-25 23:53:50 +0300

    🕊
    🕊

--- Manj_KDE.sh
+++ Manj_KDE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ZSH
+# ? ZSH
 sudo pacman -S zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
@@ -8,7 +8,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
-# PipeWire
+# ? PipeWire
 sudo pacman -Ru --nodeps pulseaudio pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-rtp pulseaudio-zeroconf pulseaudio-bluetooth pulseaudio-pa pulseaudio-alsa pulseaudio-ctl manjaro-pulse && sudo pacman -S manjaro-pipewire easyeffects pipewire
 # sudo pacman -R manjaro-pulse
 # sudo pacman -R pulseaudio-alsa pulseaudio-bluetooth pulseaudio-ctl pulseaudio-zeroconf
@@ -18,7 +18,7 @@ sudo pacman -Ru --nodeps pulseaudio pulseaudio-equalizer pulseaudio-jack pulseau
 # sudo pacman -S plasma-pa easyeffects pipewire
 
 
-# Genel KDE
+# ? Genel KDE
 sudo pacman -S yay pacui latte-dock xdg-desktop-portal-gtk telegram-desktop libxcrypt-compat -y
 sudo pacman -S optimus-manager
 pacaur -S optimus-manager-qt
@@ -28,24 +28,30 @@ pacaur -S realvnc-vnc-viewer teamviewer ruby-colorls tmuxinator -y
 sudo pacman -S brave-browser remmina thefuck micro screenfetch neofetch -y
 sudo pacman -S libvncserver freerdp  -y
 sudo pacman -S filezilla simplescreenrecorder -y
-sudo pacman -S tmux docker docker-compose android-tools rust npm ruby -y
+sudo pacman -S tmux android-tools rust npm ruby -y
 sudo pacman -S vlc libreoffice-still -y
 pacaur -S jdk -y
 
-# MongoDB
+# ? MongoDB
 pacaur -S mongodb-compass -y
 
-# Visual Studio Code
+# ? Docker
+sudo pacman -S docker docker-compose -y
+# sudo systemctl start docker.service
+# sudo systemctl enable docker.service
+# sudo usermod -aG docker $USER
+
+# ? Visual Studio Code
 sudo pacman -S snapd -y
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install code --classic
 
-# Spotify
+# ? Spotify
 yay -S extra/flatpak && flatpak install spotify
 
 
-# youtube-dl
+# ? youtube-dl
 sudo -H pip install --upgrade yt-dlp
 pip3 install vizex
 
@@ -53,13 +59,13 @@ pip3 install vizex
 
 
 
-# Conky
+# ? Conky
 sudo pacman -S conky conky-manager -y
 
-# Sistemde Kurulu Bütün Python Paketlerini Güncellemek
+# ? Sistemde Kurulu Bütün Python Paketlerini Güncellemek
 python3 -m pip install --upgrade pip
 pip install pip-review
 pip-review --local --interactive
 
-# VS-Code Eklentiler
+# ? VS-Code Eklentiler
 curl https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.vscode/extensions.txt | xargs -L 1 code --install-extension
--- for_docker/Readme.md
+++ for_docker/Readme.md
@@ -26,3 +26,54 @@ docker rm $(docker ps -a -q)
 docker image rm minirobot
 docker image rm python:3.9.5-buster
 ```
+
+## Docker içinde Dosya Düzenlemek
+
+1. Konteynırın ID'sini bulun
+
+```sh
+docker container ls
+
+CONTAINER ID   IMAGE                   COMMAND                  CREATED          STATUS          PORTS                  NAMES
+b77eac5ae3a9   wordpress:latest        "docker-entrypoint.s…"   31 minutes ago   Up 31 minutes   0.0.0.0:8080->80/tcp   wordpress-installer_wordpress_1
+685c94879f3f   phpmyadmin/phpmyadmin   "/docker-entrypoint.…"   31 minutes ago   Up 31 minutes   0.0.0.0:3000->80/tcp   wordpress-installer_phpmyadmin_1
+b74bbc5019ae   mysql:5.7               "docker-entrypoint.s…"   31 minutes ago   Up 31 minutes   3306/tcp, 33060/tcp    wordpress-installer_mysql_1
+```
+
+veya
+
+```sh
+docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'
+
+CONTAINER ID   NAMES                            IMAGE
+b77eac5ae3a9   wordpress-installer_wordpress    wordpress:latest
+685c94879f3f   wordpress-installer_phpmyadmin   phpmyadmin/phpmyadmin
+b74bbc5019ae   wordpress-installer_mysql        mysql:5.7
+```
+
+2. İstediğiniz konteynırın içine girin
+
+```sh
+sudo docker container exec -it b77eac5ae3a9 bash
+
+root@b77eac5ae3a9:/var/www/html# 
+```
+
+3. Docker imajına nano kurun
+
+```sh
+apt-get update
+apt-get install nano
+```
+
+4. Dosyayı Düzenleyin
+
+```sh
+nano /var/www/html/.htaccess
+```
+
+5. Konteynırı Yeniden Başlatın
+
+```sh
+docker restart b77eac5ae3a9
+```
