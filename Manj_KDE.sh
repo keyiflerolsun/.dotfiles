36c97ee5ec8b914023a34d7eab851d74be3a3f01
keyiflerolsun <keyiflerolsun@gmail.com>
2022-12-13 19:10:15 +0300

    🕊
    🕊

--- Manj_KDE.sh
+++ Manj_KDE.sh
@@ -1,78 +1,92 @@
 #!/bin/bash
 
+# ? Genel
+sudo pacman -S yay pacui libxcrypt-compat optimus-manager -y
+sudo pacman -S python-pip python-pipreqs python-flask python-numpy python-pandas scrapy opencv -y
+sudo pacman -S brave-browser thefuck micro screenfetch neofetch -y
+sudo pacman -S remmina libvncserver freerdp -y
+sudo pacman -S filezilla simplescreenrecorder -y
+sudo pacman -S tmux android-tools rust npm ruby jdk-openjdk -y
+sudo pacman -S jq ffmpeg -y
+
+# ? Batarya Optimizasyonu
+sudo pacman -S tlp -y
+systemctl enable tlp.service
+sudo tlp-stat -s
+
+# ? Aur
+pacaur -S optimus-manager-qt -y
+pacaur -S latte-dock-git -y
+
+# ? Python
+pip3 install --upgrade pip
+pip3 install -U setuptools wheel
+pip3 install -U yt-dlp vizex Kekik KekikSpatula SelSik thefuck
+sudo -H pip install --upgrade yt-dlp
+
+# ? Github
+git config --global user.email "keyiflerolsun@gmail.com"
+git config --global user.name "keyiflerolsun"
+git config --global credential.helper "cache --timeout=36000"
+
 # ? ZSH
 sudo pacman -S zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm -rf ~/.zshrc && wget -O ~/.zshrc https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.zshrc
 
-
-# ? PipeWire
-# sudo pacman -Ru --nodeps pulseaudio pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-rtp pulseaudio-zeroconf pulseaudio-bluetooth pulseaudio-pa pulseaudio-alsa pulseaudio-ctl manjaro-pulse && sudo pacman -S manjaro-pipewire easyeffects pipewire
-sudo pacman -R manjaro-pulse
-sudo pacman -R pulseaudio-alsa pulseaudio-bluetooth pulseaudio-ctl pulseaudio-zeroconf
-sudo pacman -R plasma-pa
-sudo pacman -R pulseaudio
-sudo pacman -S manjaro-pipewire
-sudo pacman -S plasma-pa easyeffects pipewire lsp-plugins
-
-
-# ? Genel KDE
-sudo pacman -S yay pacui xdg-desktop-portal-gtk telegram-desktop libxcrypt-compat -y
-sudo pacman -S optimus-manager
-pacaur -S optimus-manager-qt
-pacaur -S latte-dock-git
-sudo pacman -S python-pip python-pipreqs python-flask python-numpy python-pandas scrapy opencv -y
-pacaur -S python-devtools authy -y
-pacaur -S realvnc-vnc-viewer teamviewer ruby-colorls tmuxinator -y
-sudo pacman -S brave-browser remmina thefuck micro screenfetch neofetch -y
-sudo pacman -S libvncserver freerdp  -y
-sudo pacman -S filezilla simplescreenrecorder -y
-sudo pacman -S tmux android-tools rust npm ruby -y
-sudo pacman -S vlc libreoffice-still -y
-pacaur -S jdk -y
-
-# ? MongoDB
-pacaur -S mongodb-compass -y
-
-# ? Docker
-sudo pacman -S docker docker-compose -y
-# sudo systemctl start docker.service
-# sudo systemctl enable docker.service
-# sudo usermod -aG docker $USER
-
-# ? Visual Studio Code
+# ? Snap » Visual Studio Code - Authy
 sudo pacman -S snapd libpamac-snap-plugin -y
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install code --classic
+sudo snap install authy
 
-# ? Spotify
-sudo pacman -S flatpak libpamac-flatpak-plugin -y
-flatpak install spotify
-flatpak install flathub io.github.mimbrero.WhatsAppDesktop
+# ? Flatpak » Telegram - Spotify - PulseEffects - Anydesk - MongoDB - VLC - WhatsApp
 # yay -S extra/flatpak && flatpak install spotify
+sudo pacman -S flatpak libpamac-flatpak-plugin -y
+flatpak install flathub org.telegram.desktop
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.github.wwmm.pulseeffects
+flatpak install flathub com.anydesk.Anydesk
+flatpak install flathub com.mongodb.Compass
+flatpak install flathub org.videolan.VLC
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop
+
+# ? Docker
+sudo pacman -S docker docker-compose -y
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo usermod -aG docker $USER
+# docker run -d --name=portainer --restart=always -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+# docker run -d --name=mongodb --restart=unless-stopped -p 27017:27017 mongo:latest --auth
 
 
-# ? youtube-dl
-sudo -H pip install --upgrade yt-dlp
-pip3 install vizex
+# ? Swap Alanı Oluştur (16M » 16GB)
+sudo dd if=/dev/zero of=/swap bs=1024 count=16M
+sudo chmod 600 /swap
+# sudo chown root:root /swap
+sudo mkswap /swap
+sudo swapon /swap
+sudo sh -c "echo '/swap swap swap defaults 0 0' >> /etc/fstab"
+free
 
-# ? GitHub
-git config --global user.email "keyiflerolsun@gmail.com"
-git config --global user.name "keyiflerolsun"
-git config --global credential.helper "cache --timeout=36000"
+# # ? Sistemde Kurulu Bütün Python Paketlerini Güncellemek
+# python3 -m pip install --upgrade pip
+# pip install pip-review
+# pip-review --local --interactive
+
+# # ? VS-Code Eklentiler
+# curl https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.vscode/extensions.txt | xargs -L 1 code --install-extension
 
 
-
-# ? Conky
-sudo pacman -S conky conky-manager -y
-
-# ? Sistemde Kurulu Bütün Python Paketlerini Güncellemek
-python3 -m pip install --upgrade pip
-pip install pip-review
-pip-review --local --interactive
-
-# ? VS-Code Eklentiler
-curl https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.vscode/extensions.txt | xargs -L 1 code --install-extension
+# # ? PipeWire
+# # sudo pacman -Ru --nodeps pulseaudio pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-rtp pulseaudio-zeroconf pulseaudio-bluetooth pulseaudio-pa pulseaudio-alsa pulseaudio-ctl manjaro-pulse && sudo pacman -S manjaro-pipewire easyeffects pipewire
+# sudo pacman -R manjaro-pulse
+# sudo pacman -R pulseaudio-alsa pulseaudio-bluetooth pulseaudio-ctl pulseaudio-zeroconf
+# sudo pacman -R plasma-pa
+# sudo pacman -R pulseaudio
+# sudo pacman -S manjaro-pipewire
+# sudo pacman -S plasma-pa easyeffects pipewire lsp-plugins
--- Vultr_Ubuntu2204.sh
+++ Vultr_Ubuntu2204.sh
@@ -8,29 +8,32 @@ sudo apt install language-pack-tr-base -y
 
 # Yüklemek İçin: `curl https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/Vultr_Ubuntu2204.sh | bash`
 
-# ? temel
-sudo apt install language-pack-tr-base -y
+# ? Genel
 sudo apt install wget git zsh htop -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo apt install python3-dev python3-pip python3-scrapy python3-pandas -y
-pip3 install --upgrade pip
-pip3 install -U setuptools
-pip3 install -U wheel
+sudo apt install screenfetch neofetch -y
+sudo apt install jq ffmpeg -y
+sudo timedatectl set-timezone Europe/Istanbul
+# echo "KekikAkademi" > /etc/hostname
+# ulimit -n 4096
 
-# ? zsh
+# ? Python
+pip3 install --upgrade pip
+pip3 install -U setuptools wheel
+pip3 install -U yt-dlp vizex Kekik KekikSpatula SelSik thefuck
+
+# ? ZSH
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm -rf ~/.zshrc && wget -O ~/.zshrc https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.zshrc
+# chsh -s $(which zsh) && zsh
 
-# ? thefuck
-pip3 install thefuck
-sudo apt install thefuck -y
-
-# ? vizex
-pip3 install vizex
-
-# ? yt-dlp
-sudo -H pip install --upgrade yt-dlp
+# ? TMUX
+sudo apt install tmux tmuxinator -y
+rm -rf ~/.tmux.conf && wget -O ~/.tmux.conf https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.tmux.conf
 
 # ? micro
 curl https://getmic.ro | bash
@@ -44,19 +47,6 @@ sudo apt install build-essential -y
 sudo apt install libncurses5-dev -y
 sudo gem install colorls
 
-# nginx - certbot for letsencrypt | » https://nginxproxymanager.com/guide/#quick-setup «
-# sudo apt install nginx certbot python3-certbot-nginx
-# sudo systemctl start nginx
-# sudo nginx -t
-# sudo systemctl reload nginx
-# sudo ufw allow 'Nginx Full'
-# sudo certbot --nginx
-
-## micro /etc/nginx/nginx.conf
-## sudo certbot --nginx -d plusbinance.com
-## sudo nginx -t && sudo nginx -s reload
-## sudo service nginx restart
-
 # ? Swap Alanı Oluştur (3M » 3GB)
 sudo dd if=/dev/zero of=/swap bs=1024 count=3M
 sudo chmod 600 /swap
@@ -67,25 +57,10 @@ sudo sh -c "echo '/swap swap swap defaults 0 0' >> /etc/fstab"
 free
 
 # ? Docker
-## https://github.com/keyiflerolsun/.dotfiles/blob/main/for_docker/Readme.md#dockerfile-yenir-mi
 sudo apt install docker docker-compose -y
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
-
-docker run -d --name=portainer --restart=always -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-docker run -d --name=mongodb --restart=unless-stopped -p 27017:27017 mongo:latest --auth
-docker run -d --name=nginx-proxy-manager --restart=unless-stopped -p 80:80 -p 81:81 -p 443:443 -v /root/nginx-proxy-manager/data:/data -v /root/nginx-proxy-manager/letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
-
-# ? keyiflerolsun
-rm -rf ~/.zshrc && wget https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.zshrc
-# echo "KekikAkademi" > /etc/hostname
-sudo timedatectl set-timezone Europe/Istanbul
-sudo apt install screenfetch neofetch tmux tmuxinator -y
-wget https://raw.githubusercontent.com/keyiflerolsun/.dotfiles/main/.tmux.conf
-sudo apt install jq ffmpeg -y
-ulimit -n 4096
-git config --global user.email "keyiflerolsun@gmail.com"
-git config --global user.name "keyiflerolsun"
-git config --global credential.helper "cache --timeout=36000"
-chsh -s $(which zsh) && zsh
+# docker run -d --name=portainer --restart=always -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+# docker run -d --name=mongodb --restart=unless-stopped -p 27017:27017 mongo:latest --auth
+# docker run -d --name=nginx-proxy-manager --restart=unless-stopped -p 80:80 -p 81:81 -p 443:443 -v /root/nginx-proxy-manager/data:/data -v /root/nginx-proxy-manager/letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
