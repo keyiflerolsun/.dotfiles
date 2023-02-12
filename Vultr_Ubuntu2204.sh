203b0fb0185ae5e5dd9210f05136cb2d5a005415
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2023-02-12 15:05:06 +0300

    Update Vultr_Ubuntu2204.sh

--- Vultr_Ubuntu2204.sh
+++ Vultr_Ubuntu2204.sh
@@ -49,13 +49,13 @@ sudo apt install libncurses5-dev -y
 sudo gem install colorls
 
 # ? Swap Alanı Oluştur (3M » 3GB)
-sudo dd if=/dev/zero of=/swap bs=1024 count=3M
-sudo chmod 600 /swap
+# sudo dd if=/dev/zero of=/swap bs=1024 count=3M
+# sudo chmod 600 /swap
 # sudo chown root:root /swap
-sudo mkswap /swap
-sudo swapon /swap
-sudo sh -c "echo '/swap swap swap defaults 0 0' >> /etc/fstab"
-free
+# sudo mkswap /swap
+# sudo swapon /swap
+# sudo sh -c "echo '/swap swap swap defaults 0 0' >> /etc/fstab"
+# free
 
 # ? Docker
 sudo apt install docker docker-compose -y
