d2ae671ebb9ea974f589ab2278029b88b3a6ffa1
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2023-03-04 22:36:26 +0300

    Update Vultr_Ubuntu2204.sh

--- Vultr_Ubuntu2204.sh
+++ Vultr_Ubuntu2204.sh
@@ -84,6 +84,7 @@ sudo ip6tables -t nat -A POSTROUTING -s fd00::/80 ! -o docker0 -j MASQUERADE
 sudo apt-get install iptables-persistent -y
 sudo iptables-save > /etc/iptables/rules.v4
 sudo ip6tables-save > /etc/iptables/rules.v6
+sudo ufw allow tcp
 
 ## * https://github.com/keyiflerolsun/docker-compose_Yenir_Mi
 # docker run -d --name=portainer --restart=always -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
