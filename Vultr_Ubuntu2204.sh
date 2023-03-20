e7d00989b199bf53589b3664a54bd53d92393ffc
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2023-03-20 16:05:21 +0300

    Update Vultr_Ubuntu2204.sh

--- Vultr_Ubuntu2204.sh
+++ Vultr_Ubuntu2204.sh
@@ -63,8 +63,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-# sudo systemctl start docker.service
-# sudo systemctl enable docker.service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 sudo usermod -aG docker $USER
 
 ## * Docker IPv6 -- https://medium.com/@skleeschulte/how-to-enable-ipv6-for-docker-containers-on-ubuntu-18-04-c68394a219a2
