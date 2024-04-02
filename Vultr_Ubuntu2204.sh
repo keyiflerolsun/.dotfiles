1b2606ac284fce57e210dcbc823413190d8256a4
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2024-04-02 12:23:44 +0300

    Update Vultr_Ubuntu2204.sh

--- Vultr_Ubuntu2204.sh
+++ Vultr_Ubuntu2204.sh
@@ -78,9 +78,20 @@ sudo usermod -aG docker $USER
 cat >>/etc/docker/daemon.json <<EOF
 
 {
+  "data-root": "/data/docker/",
+  "log-driver": "json-file",
+  "log-opts": { "max-size": "10m", "max-file": "2" },
   "ipv6": true,
-  "fixed-cidr-v6": "fd00::/80"
+  "fixed-cidr-v6": "2001:db8:1::/64",
+  "experimental": true,
+  "ip6tables": true,
+  "default-address-pools": [
+    { "base": "172.17.0.0/16", "size": 24 },
+    { "base": "192.168.0.0/16", "size": 24 },
+    { "base": "2001:db8::/104", "size": 112 }
+  ]
 }
+
 
 EOF
 
