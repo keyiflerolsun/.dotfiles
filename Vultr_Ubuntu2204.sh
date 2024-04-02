f385d8c167def9608fc9f296755aae3de8fc5858
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2024-04-02 16:21:52 +0300

    Update Vultr_Ubuntu2204.sh

--- Vultr_Ubuntu2204.sh
+++ Vultr_Ubuntu2204.sh
@@ -83,12 +83,7 @@ cat >>/etc/docker/daemon.json <<EOF
   "ipv6": true,
   "fixed-cidr-v6": "fd00::/64",
   "experimental": true,
-  "ip6tables": true,
-  "default-address-pools": [
-    { "base": "172.17.0.0/16", "size": 24 },
-    { "base": "192.168.0.0/16", "size": 24 },
-    { "base": "fd00::/64", "size": 64 }
-  ]
+  "ip6tables": true
 }
 
 EOF
