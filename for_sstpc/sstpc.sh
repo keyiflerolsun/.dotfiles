d05673db1a7b8fc6c2767d36f97e5185509a1bdc
keyiflerolsun <keyiflerolsun@gmail.com>
2025-10-10 23:41:15 +0300

    🕊
    🕊

--- for_sstpc/sstpc.sh
+++ for_sstpc/sstpc.sh
@@ -26,11 +26,6 @@ SIFRE=$2
 DOMAIN=$3
 
 
-# ? Yerel ağ aralığını tespit et
-YEREL_AG=$(ip route | grep '^default' | grep -oP 'dev \K\w+' | xargs -I{} ip addr show {} | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1 | cut -d'.' -f1-3 | awk '{print $1".0/24"}')
-echo -e "${YELLOW}[i] Yerel ağ aralığı tespit edildi: ${CYAN}$YEREL_AG${NC}"
-
-
 # ? VPN bağlantısını başlat ve PID'yi al
 echo -e "${YELLOW}[~] VPN bağlantısı kuruluyor...${NC}"
 sudo sstpc --log-level 4 --log-stderr --cert-warn --tls-ext --user "$KULLANICI_ADI" --password "$SIFRE" "$DOMAIN" usepeerdns require-mschap-v2 noauth noipdefault noccp refuse-eap refuse-pap refuse-mschap &
@@ -45,16 +40,25 @@ sleep 5
 VPN_ARAYUZ=$(ip addr show | grep 'inet.*ppp' | awk '{print $NF}' | head -n 1)
 
 
-# ? VPN IP adresini al ve rotayı ekle
+# ? VPN IP adresini, gateway'i ve subnet'i al
 if [[ ! -z "$VPN_ARAYUZ" ]]; then
     VPN_IP=$(ip addr show $VPN_ARAYUZ | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+    VPN_GATEWAY=$(ip route | grep -m 1 "$VPN_ARAYUZ" | awk '{print $1}' | cut -d'/' -f1)
+    VPN_SUBNET=$(echo $VPN_GATEWAY | cut -d'.' -f1-3).0/16
+    # VPN_SUBNET=$(echo $VPN_GATEWAY | cut -d'.' -f1-3).0/24
 
     echo -e "${GREEN}[+] VPN bağlantısı kuruldu${NC}"
-    echo -e "${YELLOW}[i] VPN IP adresi: $VPN_IP${NC}"
-    echo -e "${YELLOW}[i] VPN arayüzü: $VPN_ARAYUZ${NC}"
+    echo -e "${YELLOW}[i] VPN IP     : $VPN_IP${NC}"
+    echo -e "${YELLOW}[i] Gateway IP : $VPN_GATEWAY${NC}"
+    echo -e "${YELLOW}[i] SubNet     : $VPN_SUBNET${NC}"
 
-    sudo ip route add $YEREL_AG via $VPN_IP dev $VPN_ARAYUZ
-    echo -e "${GREEN}[+] ${CYAN}$YEREL_AG${GREEN} ağı ${CYAN}$VPN_IP${GREEN} üzerinden route edildi.${NC}"
+    # Rota zaten varsa eklemeye çalışmadan önce kontrol et
+    if ip route show | grep -q "$VPN_SUBNET via $VPN_IP"; then
+        echo -e "${YELLOW}[i] Rota zaten mevcut: $VPN_SUBNET via $VPN_IP${NC}"
+    else
+        sudo ip route add $VPN_SUBNET via $VPN_IP dev $VPN_ARAYUZ
+        echo -e "${GREEN}[+] ${CYAN}$VPN_SUBNET${GREEN} ağı ${CYAN}$VPN_IP${GREEN} üzerinden route edildi.${NC}"
+    fi
 else
     echo -e "${RED}[!] VPN bağlantısı başarısız oldu ya da PPP arayüzü oluşturulamadı.${NC}"
     exit 1
@@ -64,10 +68,10 @@ fi
 # ? Temizlik fonksiyonu
 temizlik() {
     echo -e "${PURPLE}[~] VPN bağlantısı kapatılıyor ve yönlendirme kaldırılıyor...${NC}"
-    
+
     # ? Rota mevcutsa kaldır
-    if ip route show | grep -q "$YEREL_AG via $VPN_IP"; then
-        sudo ip route del $YEREL_AG via $VPN_IP dev $VPN_ARAYUZ
+    if ip route show | grep -q "$VPN_SUBNET via $VPN_IP"; then
+        sudo ip route del $VPN_SUBNET via $VPN_IP dev $VPN_ARAYUZ
         echo -e "${CYAN}[-] Yönlendirme kaldırıldı.${NC}"
     fi
 
